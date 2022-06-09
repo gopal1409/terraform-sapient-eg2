909bce3c1afc0d6d40ddbfd9701793d19e17f88b
John <gopal1409@gmail.com>
2022-06-09 10:05:00 +0530

    terraform provider with random generate download

---
+++ c1-version.tf
@@ -0,0 +1,22 @@
+#terraform init to do the same we will create provider block., 
+#infrastructure as code do not change frequently
+#when we do terrafomr init it need to download all the plugins required to run deploy your application in the cloud
+##create a provider block which contains 
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+    random = {
+        source = "hashicorp/azurerm"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
